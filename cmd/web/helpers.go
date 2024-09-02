b5d1458b94af2ce3f4f9c10a3fb1789193cb52f8
NUNO MARTINS <nuno.martins@mercedes-benz.com>
2024-09-02 10:23:33 +0100

    first commit

---
+++ cmd/web/helpers.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"net/http"
+	"runtime/debug"
+	"time"
+
+	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
+)
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	isAuthenticated, ok := r.Context().Value(isAthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+
+	return isAuthenticated
+}
+
+func (app *application) decodePostForm(r *http.Request, dst any) error {
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	err = app.formDecoder.Decode(dst, r.PostForm)
+	if err != nil {
+		var invalidDecoderError *form.InvalidDecoderError
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		return err
+	}
+
+	return nil
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
+	// Initialize a new buffer.
+	buf := new(bytes.Buffer)
+	// Write the template to the buffer, instead of straight to the
+	// http.ResponseWriter. If there's an error, call our serverError() helper // and then return.
+	err := ts.ExecuteTemplate(buf, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(status)
+
+	// If the template is written to the buffer without any errors, we are safe // to go ahead and write the HTTP status code to http.ResponseWriter. w.WriteHeader(status)
+	// Write the contents of the buffer to the http.ResponseWriter. Note: this // is another time where we pass our http.ResponseWriter to a function that // takes an io.Writer.
+	buf.WriteTo(w)
+}
+
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+		trace  = debug.Stack()
+	)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// Create an newTemplateData() helper, which returns a pointer to a templateData // struct initialized with the current year. Note that we're not using the
+// *http.Request parameter here at the moment, but we will do later in the book.
+func (app *application) newTemplateData(r *http.Request) templateData {
+	return templateData{
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
+	}
+}
---
+++ cmd/web/templates.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"html/template"
+	"io/fs"
+	"path/filepath"
+	"time"
+
+	"github.com/ngmmartins/snippetbox/internal/models"
+	"github.com/ngmmartins/snippetbox/ui"
+)
+
+type templateData struct {
+	CurrentYear     int
+	Snippet         models.Snippet
+	Snippets        []models.Snippet
+	Form            any
+	Flash           string
+	IsAuthenticated bool
+	CSRFToken       string
+}
+
+// Create a humanDate function which returns a nicely formatted string // representation of a time.Time object.
+func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.UTC().Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is // essentially a string-keyed map which acts as a lookup between the names of our // custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// Initialize a new map to act as the cache.
+	cache := map[string]*template.Template{}
+
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+	// Loop through the page filepaths one-by-one.
+	for _, page := range pages {
+		// Extract the file name (like 'home.tmpl') from the full filepath // and assign it to the name variable.
+		name := filepath.Base(page)
+
+		patterns := []string{
+			"html/base.tmpl",
+			"html/partials/*.tmpl",
+			page,
+		}
+
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+	// Return the map.
+	return cache, nil
+}
